f5bd75615fa6a1d7b0fb91ba616288cb3b84ce0e
venkatasatishreddy <venkatasatishreddy.s@gmail.com>
2021-06-08 20:27:46 +0530

    Dynamic Filtering.

--- SampleApplication/src/main/java/com/innobright/ws/filtering/ControllerForFilter.java
+++ SampleApplication/src/main/java/com/innobright/ws/filtering/ControllerForFilter.java
@@ -3,20 +3,48 @@ package com.innobright.ws.filtering;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 
 @RestController
 public class ControllerForFilter {
 	
 	@GetMapping(path = "/filtering")
-	public FilterClass getFilter() {
-		return new FilterClass("v1", "v2", "v3");
+	public MappingJacksonValue getFilter() {
+		FilterClass filterClass = new FilterClass("v1", "v2", "v3");
+//		By using below class we can set what are the propeties have to set in a response.
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("var1", "var2");
+		
+//		In below stmt we have mentioned on which obj we have to apply MappingJacksonValue.
+//		MappingJacksonValue mappingValue = new MappingJacksonValue(filterClass);
+//		FilterProvider filterProvider = new SimpleFilterProvider().addFilter("FilterBean", filter);
+//		mappingValue.setFilters(filterProvider);		
+		
+		return getMappingJacksonValue(filter, filterClass, null);
 	}
 	
 	@GetMapping(path = "/list-filtering")
-	public List<FilterClass> getFilters() {
-		return Arrays.asList(new FilterClass("v1", "v2", "v3"), new FilterClass("v1", "v2", "v3"));
+	public MappingJacksonValue getFilters() {
+		List<FilterClass> asList = Arrays.asList(new FilterClass("v1", "v2", "v3"), new FilterClass("v1", "v2", "v3"));
+		
+		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("var2", "var3");
+		
+//		MappingJacksonValue mappingValue = new MappingJacksonValue(asList);
+//		FilterProvider filterProvider = new SimpleFilterProvider().addFilter("FilterBean", filter);
+//		mappingValue.setFilters(filterProvider);
+		return getMappingJacksonValue(filter, null, asList);
+	}
+	
+	public MappingJacksonValue getMappingJacksonValue(SimpleBeanPropertyFilter filter, FilterClass filterClass, List<FilterClass> asList) {
+		MappingJacksonValue mappingValue = filterClass != null ? new MappingJacksonValue(filterClass) : new MappingJacksonValue(asList);
+		FilterProvider filterProvider = new SimpleFilterProvider().addFilter("FilterBean", filter);
+		mappingValue.setFilters(filterProvider);
+		return mappingValue;
 	}
 
 }
--- SampleApplication/src/main/java/com/innobright/ws/filtering/FilterClass.java
+++ SampleApplication/src/main/java/com/innobright/ws/filtering/FilterClass.java
@@ -1,5 +1,6 @@
 package com.innobright.ws.filtering;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -7,15 +8,21 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
  * Static Filtering:-
  * If we don't want to send some of the properties in response we can utilize JsonIgnore and JsonIgnoreProperties
  * But JsonIgnore is recommended  bcz if any var name change again we have to change in JsonIgnoreProperties
+ * 
+ * Dynamic Filtering:-
+ * It means in one scenario required var1 and var2 and other scenario required var2 and var3.
+ * For this we have to utilize MappinJacksonValue.
+ * For Dynamic filtering we have to add @JsonFilter(value = "somename") on class, otherwise filter won't work
  */
 
 //@JsonIgnoreProperties({"var2", "var3"})
+@JsonFilter(value = "FilterBean")
 public class FilterClass {
 
 	private String var1;
-	@JsonIgnore
+//	@JsonIgnore
 	private String var2;
-	@JsonIgnore
+//	@JsonIgnore
 	private String var3;
 
 	public FilterClass(String var1, String var2, String var3) {
